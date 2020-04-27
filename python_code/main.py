fbaefd879bcfeee8b2c1426d904ca646d40fd2e1
Antoś Bućko <anton.butsko@gmail.com>
2020-04-27 11:15:35 +0300

    Add threads

--- python_code/main.py
+++ python_code/main.py
@@ -6,10 +6,18 @@ from recalculation import Recalculation
 from router import Router
 
 
-csr = Router('172.31.1.10', 'dranser', 'cisco')
-routers = [csr]
-recalculation = Recalculation(0.8, 10)
+router1 = Router('172.31.1.10', 'dranser', 'cisco')
+routers = [router1]
+
+recalculation = []
+for router in routers:
+    recalculation.append(Recalculation(router, 0.8, 10))
 
 while True:
-    recalculation.recalculate(routers)
+    for thread in recalculation:
+        thread.start()
+
+    for thread in recalculation:
+        thread.join()
+
     time.sleep(Recalculation.TIMEOUT)
--- python_code/recalculation.py
+++ python_code/recalculation.py
@@ -1,29 +1,31 @@
+import threading
 from router import Router
 
 
-class Recalculation:
+class Recalculation(threading.Thread):
     TIMEOUT = 5
 
-    def __init__(self, coefficient, threshold):
+    def __init__(self, router, coefficient, threshold):
+        super().__init__()
+        self.router = router
         self.coefficient = coefficient
         self.threshold = threshold
 
-    def recalculate(self, routers):
-        for router in routers:
-            interfaces = router.get_interfaces()
-            for interface in interfaces:
-                name = interface.get_name()
-                old_txload = interface.get_txload()
-                current_txload = router.get_interface_txload(name)
+    def run(self):
+        interfaces = self.router.get_interfaces()
+        for interface in interfaces:
+            name = interface.get_name()
+            old_txload = interface.get_txload()
+            current_txload = self.router.get_interface_txload(name)
 
-                new_txload = round(self.coefficient * old_txload + (1 - self.coefficient) * current_txload)
+            new_txload = round(self.coefficient * old_txload + (1 - self.coefficient) * current_txload)
 
-                if abs(new_txload - old_txload) > self.threshold:
-                    interface.set_txload(current_txload)
-                    router.restart_eigrp()
-                    print('Metric is recalculated for router: {router}'.format(router=router.get_host()))
-                    print('Old Load: {old_load}; New Load: {new_load}'.format(old_load=old_txload, new_load=current_txload))
-                    break
-                else:
-                    print('Load change on {interface_name} is less than threshold'.format(interface_name=name))
-                    print('New Load: {new_load}; Old Load: {old_load}'.format(new_load=new_txload, old_load=old_txload))
+            if abs(new_txload - old_txload) > self.threshold:
+                interface.set_txload(current_txload)
+                self.router.restart_eigrp()
+                print('Metric is recalculated for router: {router}'.format(router=self.router.get_host()))
+                print('Old Load: {old_load}; New Load: {new_load}'.format(old_load=old_txload, new_load=current_txload))
+                break
+            else:
+                print('Load change on {interface_name} is less than threshold'.format(interface_name=name))
+                print('New Load: {new_load}; Old Load: {old_load}'.format(new_load=new_txload, old_load=old_txload))
